1a70be0e20bd2a6668034b1442806a23ccce80d3
George Kemp <gkemp@hubspot.com>
2020-06-30 11:50:34 -0400

    Update

--- scripts/saveCredentials.js
+++ scripts/saveCredentials.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
 const { DB_CREDENTIALS } = process.env;
-
+console.log(DB_CREDENTIALS);
+console.log(process.env);
 fs.writeFileSync('./scripts/credentials.json', DB_CREDENTIALS);
