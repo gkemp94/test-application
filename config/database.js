8dcd679ced805ee95a6e46033fa787a0324e0e3f
George Kemp <gkemp@hubspot.com>
2020-07-01 12:29:14 -0400

    Take 2

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ module.exports = ({ env }) => {
             port,
             username, 
             password,
-            database: 'test-db'
+            database: 'strapi'
           },
         },
       },
